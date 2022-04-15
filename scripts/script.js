d59e8b12eae24169353e702ec9cbf791e7d0f8bc
danton03 <37675973+danton03@users.noreply.github.com>
2022-04-15 16:22:21 -0300

    :ballot_box_with_check: Manter o usuário conectado

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,36 @@
-/* Perguntar no nome do usuário */
+/*                          Entrando na sala do chat                      */
 let statusCode = 0;
-let deuCerto = [];
 function entrarNaSala() {
+  //Pergunta o nome do usuário 
   const nome = prompt(`Bem-vindo ao Bate-papo UOL!\nQual o seu nome?`);
-  const usuario = { name: nome }
+  const usuario = { name: nome };
+
+  //Zera a variável statusCode em caso de uma repetição dessa função
   statusCode = 0;
+
+  //Faz a requisição para colocar o usuário no chat
   axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario)
-  .then(conexaoAceita)
-  .catch(falha)
+    .then(conexaoAceita) //se der certo
+    .catch(falhaNaConexao) //se der erro
 }
+
+/*                     Tratando as respostas do servidor                  */
 
 function conexaoAceita(resposta) {
-  console.log(resposta);
+  /* Pegando o nome do usuário na resposta da requisição (que vem em JSON),
+     convertendo para objeto e pegando apena o valor
+  */
+  const usuarioConectado = JSON.parse(resposta.config.data).name;
+
+  /* Atualizando o status de conectado do usuário a cada 4s */
+  function manterOnline() {
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: usuarioConectado });
+  }
+  setInterval(manterOnline, 4000);
 }
 
-function falha(erro) {
+function falhaNaConexao(erro) {
+  //Pega o status code do erro e verifica se foi por nome já existente
   statusCode = erro.response.status;
   if (statusCode === 400) {
     entrarNaSala();
